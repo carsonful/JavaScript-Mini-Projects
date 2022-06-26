109aa80487a506f19bafd6b2977315c3b5752032
Carson Fulmer <94324583+carsonful@users.noreply.github.com>
2022-06-26 12:03:10 -0700

    Add files via upload

---
+++ DogYears.js
@@ -0,0 +1,15 @@
+const myAge = 17;
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+let laterYears = myAge -2;
+
+laterYears *=4;
+// gets my age in dog years
+var myAgeInDogYears=earlyYears + laterYears;
+
+var name = "Carson Ful";
+
+
+console.log(`My name is ${name}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
